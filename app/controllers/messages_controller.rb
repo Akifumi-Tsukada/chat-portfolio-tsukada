c38fcfc8540d04caa1a11f3192ac0c848c6a6c23
Tsukada-Akifumi <a.tsuka0190@gmail.com>
2021-08-10 18:37:13 +0900

    add function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,20 @@ class MessagesController < ApplicationController
   def new
     @messages = Message.all
     @message = Message.new
+    @users = User.all
   end
 
   def create
-    @message = Message.new(text: params[:message][:text])
+    @message = Message.new(message_params)
     if @message.save
       ActionCable.server.broadcast 'message_channel', content: @message
     end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'messages#new'
   resources :messages, only: [:create]
+  resources :users, only: [:edit, :update]
 end
